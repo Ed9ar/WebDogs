eddd551f75552736e9bf1e0507fa000e5cc7f9de
EmilianoR <emiroldanp@hotmail.com>
2020-10-10 11:47:45 -0500

    Creacion de componente

--- Dogs/proyecto-webdogs/src/app/app.module.ts
+++ Dogs/proyecto-webdogs/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './main-components/encabezado/encabezado.component';
 import { FooterComponent } from './main-components/footer/footer.component';
+import { ContentComponent } from './main-components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     FooterComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
