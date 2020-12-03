54ad0cc9e48ae7534ee0a8e81bdf315b2b3567c3
ed9ar <a01339939@itesm.mx>
2020-12-02 20:38:12 -0600

    insertarPerros
    Edgar López Valdés A01339939

--- Dogs/proyecto-webdogs/src/app/module/registrar-perro/services/registrar-perro.service.ts
+++ Dogs/proyecto-webdogs/src/app/module/registrar-perro/services/registrar-perro.service.ts
@@ -1,21 +1,96 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Perro } from '../../../models/perro.model';
+import { Observable, of, throwError } from 'rxjs';
+
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+
+import { map, retry, catchError, tap } from 'rxjs/operators';
+import { $ } from 'protractor';
+import { createIncrementalCompilerHost } from 'typescript';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrarPerroService {
 
+  endpoint = 'http://localhost:3000/api/perro';
+
+  constructor(private http: HttpClient){}
+
+  private extraData(res: Response){
+    let body = res;
+
+    return body || {};
+  }
+
+  /*
+  private perroAgregado = new Subject<object>();
+
+  clasePerroAgregadoAnunciada$ = this.perroAgregado.asObservable();
+  */
+  
+  handleError(error: HttpErrorResponse){
+    let errorMessage = 'Unknown error';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = `Error: ${error.error.message}`;
+    }else{
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage); //Meter gui bonitas
+    return throwError(errorMessage);
+  }
+
+  
+  
+  getPerros(){
+    console.log("en el servicio");
+    return this.http.get<Perro[]>(this.endpoint).pipe(retry(3), catchError(this.handleError));
+  }
+
+  
   private perroAgregado = new Subject<object>();
 
   clasePerroAgregadoAnunciada$ = this.perroAgregado.asObservable();
 
-  agregacionPerro(perro: object){
-    this.perroAgregado.next(perro);
-    console.log("Perro agregado");
-    console.log(perro);
-  }
-  
 
- 
+  insertarPerro(perros: Perro){
+    this.http.post<Perro>(this.endpoint, perros).subscribe({
+      next: data =>{
+        console.log("datos", data);
+      },error: error =>{
+        console.error( ' error!', error);
+      }
+      
+    })
+  }
+
+  /*
+  updatePerro(perros: Perro, id:String){
+    this.http.put<Perro>(this.endpoint+"/"+id, perros).subscribe({
+      next: data =>{
+        console.log("datos", data);
+      },error: error =>{
+        console.error( ' error!', error);
+      }
+      
+    })
+  }
+
+  deletePerro(id:String){
+    this.http.delete(this.endpoint+"/"+id).subscribe({
+      next: data =>{
+        console.log("datos", data);
+      },error: error =>{
+        console.error( ' error!', error);
+      }
+      
+    })
+  }*/
 }
