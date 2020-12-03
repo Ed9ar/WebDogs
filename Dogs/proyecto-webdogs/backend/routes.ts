f2e0ad41a5541d07f98ea1924b13c69acbba2b54
ed9ar <a01339939@itesm.mx>
2020-12-02 18:32:37 -0600

    perroController
    Edgar López Valdés A01339939

--- Dogs/proyecto-webdogs/backend/routes.ts
+++ Dogs/proyecto-webdogs/backend/routes.ts
@@ -5,6 +5,14 @@ function setRoutes(app):void{
     const router = express.Router();
     const perroController = new PerroController();
     router.route("/perro").get(perroController.getAll);
+<<<<<<< Updated upstream
+=======
+    router.route("/perros/count").get(perroController.count);
+    router.route("/perro").post(perroController.insert);
+    router.route("/perro/:id").get(perroController.get);
+    router.route("/perro/:id").put(perroController.update);
+    router.route("/perro/:id").delete(perroController.delete);
+>>>>>>> Stashed changes
     app.use('/api', router);
 }
 
