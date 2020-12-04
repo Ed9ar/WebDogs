8109a765f54f9ccbc592f3be2fc53914bfed6b6f
AlanMendoza09 <37988489+AlanMendoza09@users.noreply.github.com>
2020-12-04 13:51:37 -0600

    Validaciones Modelos Mongoose

--- Dogs/proyecto-webdogs/backend/models/denuncia.ts
+++ Dogs/proyecto-webdogs/backend/models/denuncia.ts
@@ -1,14 +1,48 @@
 import * as mongoose from 'mongoose';
 
 const denuncias = new mongoose.Schema({
-    fecha: String,
-    descripcion: String,
-    responsableDenuncia: String,
-    ubicacion: String,
-    estatus: String,
-    correoContacto: String
+    fecha: {
+    type: String,
+    required: [true, "La fecha es requerida"]
+    },
+    descripcion: {
+      type: String,
+      minlength: 4,
+      maxlength: 255,
+      required: [true, "La descripción debe tener 4 caracteres como mínimo y máximo 255 caracteres"],
+    },
+    responsableDenuncia: {
+      type: String,
+      minlength: 2,
+      maxlength: 255,
+      required: [true, "El nombre del responsable debe tener más de 2 caracteres y no debe tener números"],
+    },
+    ubicacion: {
+      type: String,
+      minlength: 2,
+      maxlength: 255,
+      required: [true, "La ubicación es requerida y debe tener entre 2 y 255 caracteres"],
+    },
+    estatus: {
+      type: String,
+      minlength: 2,
+      maxlength: 255,
+      required: [true, "El estatus es requerido y debe tener entre 2 y 255 caracteres"],
+    },
+    correoContacto: {
+      type: String,
+      minlength: 4,
+      maxlength: 255,
+      required: [true, "Email no puede estar vacío"],
+    },
 });
 
 const Denuncia = mongoose.model("Denuncia", denuncias);
 
 export default Denuncia;
+
+denuncias.path('correoContacto').validate(function (correoContacto) {
+  var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailRegex.test(correoContacto.email);
+}, 'Email no puede estar vacio y debe ser válido')
+
--- Dogs/proyecto-webdogs/backend/models/perro.ts
+++ Dogs/proyecto-webdogs/backend/models/perro.ts
@@ -1,14 +1,46 @@
 import * as mongoose from 'mongoose';
 
 const perros = new mongoose.Schema({
-    nombre: String,
-    raza: String,
-    edad: Number,
-    descripcion: String,
-    correoContacto: String,
-    //url:String
+  nombre: {
+    type: String,
+    minlength: 2,
+    maxlength: 255,
+    required: [true, "El nombre debe tener más de 2 caracteres y no debe tener números"],
+  },
+  raza: {
+    type: String,
+    required: [true, "La raza es requerida"],
+
+  },
+  edad: {
+    type: Number,
+    min: 1,
+    max: 25,
+    required: [true, "La edad mínima es 1 aunque tenga meses el perro"],
+
+  },
+  descripcion: {
+    type: String,
+    minlength: 4,
+    maxlength: 255,
+    required: [true, "La descripción debe tener 4 caracteres como mínimo y máximo 255 caracteres"],
+
+  },
+  correoContacto: {
+    type: String,
+    minlength: 4,
+    maxlength: 255,
+    required: [true, "Email no puede estar vacío"],
+
+  },
+  //url: String
 });
 
 const Perro = mongoose.model("Perro", perros);
 
 export default Perro;
+
+perros.path('correoContacto').validate(function (correoContacto) {
+  var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailRegex.test(correoContacto.email);
+}, 'Email no puede estar vacio y debe ser válido')
