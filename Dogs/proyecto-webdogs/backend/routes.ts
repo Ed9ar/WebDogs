997e3322bd24de0c369857170315c116aa5d1bf9
EmilianoR <emiroldanp@hotmail.com>
2020-12-04 13:35:08 -0600

    jwt implementation, missing authorization
    Emiliano Roldan - A01650141

--- Dogs/proyecto-webdogs/backend/routes.ts
+++ Dogs/proyecto-webdogs/backend/routes.ts
@@ -1,12 +1,46 @@
 import * as express from 'express';
+import { Express } from 'express-serve-static-core';
 import PerroController from './controllers/perroController';
 import DenunciaController from './controllers/denunciaController';
 
-function setRoutes(app):void{
+//jwt
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');/// npm
+///fin de jwt npm install jsonwebtoken
+
+function setRoutes(app: Express):void{
     const router = express.Router();
     const perroController = new PerroController();
-    router.route("/perro").get(perroController.getAll);
 
+    router.use((req, res, next) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      if (req.method === "OPTIONS") {
+        res.set(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+      }
+      next();
+      }); //funcion habilita el middleware
+
+    var jwtCheck = jwt({
+          secret: jwks.expressJwtSecret({
+              cache: true,
+              rateLimit: true,
+              jwksRequestsPerMinute: 5,
+              jwksUri: 'https://dev-55arag8d.us.auth0.com/.well-known/jwks.json'
+        }),
+        audience: 'http://localhost:3000/',
+        issuer: 'https://dev-55arag8d.us.auth0.com/',
+        algorithms: ['RS256']
+    });
+
+    router.route("/perro").get(perroController.getAll);
     router.route("/perros/count").get(perroController.count);
     router.route("/perro").post(perroController.insert);
     router.route("/perro/:id").get(perroController.get);
@@ -18,6 +52,8 @@ function setRoutes(app):void{
     router.route("/denuncia").get(denunciaController.getAll);
     router.route("/denuncia/count").get(denunciaController.count);
     router.route("/denuncia").post(denunciaController.insert);
+
+    app.use(jwtCheck);
     app.use('/api', router);
 }
 
