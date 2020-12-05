941f7e7cc84b0d8ce46013fb782d778ecc988659
EmilianoR <emiroldanp@hotmail.com>
2020-12-05 01:17:52 -0600

    Auth token
    Emiliano Roldan - A01650141

--- Dogs/proyecto-webdogs/src/app/main-services/perros.service.ts
+++ Dogs/proyecto-webdogs/src/app/main-services/perros.service.ts
@@ -51,8 +51,11 @@ export class PerrosService {
 
   getPerros(){
 
+    const headers = { 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpqN3h0aEtwX1N1QlloaG4waThWVSJ9.eyJpc3MiOiJodHRwczovL2Rldi01NWFyYWc4ZC51cy5hdXRoMC5jb20vIiwic3ViIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW5AY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMC8iLCJpYXQiOjE2MDcxMzUzNzUsImV4cCI6MTYwNzIyMTc3NSwiYXpwIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW4iLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.g2R9xXeyKdoOC_9Q1nHCbAwrNqEbXdUjQEup04g1kdkepx0f2Q38imqdjdYi02ZddeWQhca4RnpUzPhcfYR3L7uJemV_OWEwc3UiqHjyFEUhFi3kqZ1CoYPe4dV9g6YHD4NYMCSP1fnqBBiow2khO0XHotr5F_ogKDNAk8UbiMyxCbvLjXLrFBSL-Vw0EgUOZXAbHTmoEjffUQNqgToDakpQLRD8JNnejpnJTO_7x521A-_s2buGZeihQSaD0xo7FeKSidjhwaUYCaqhlrHwqJObQXo6n48EIg4VixIIksdxQlGaCzWaTr0rJbxpscNSiRWmJwWQkrbVnfIp3Bknyg`, "Content-Type": "application/x-www-form-urlencoded" }
+
+
     console.log("en el servicio");
-    return this.http.get<Perro[]>(this.endpoint).pipe(retry(3), catchError(this.handleError));
+    return this.http.get<Perro[]>(this.endpoint, {headers}).pipe(retry(3), catchError(this.handleError));
   }
 
 
@@ -62,8 +65,10 @@ export class PerrosService {
 
 
   insertarPerro(perros: Perro){
+    const headers = { 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpqN3h0aEtwX1N1QlloaG4waThWVSJ9.eyJpc3MiOiJodHRwczovL2Rldi01NWFyYWc4ZC51cy5hdXRoMC5jb20vIiwic3ViIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW5AY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMC8iLCJpYXQiOjE2MDcxMzUzNzUsImV4cCI6MTYwNzIyMTc3NSwiYXpwIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW4iLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.g2R9xXeyKdoOC_9Q1nHCbAwrNqEbXdUjQEup04g1kdkepx0f2Q38imqdjdYi02ZddeWQhca4RnpUzPhcfYR3L7uJemV_OWEwc3UiqHjyFEUhFi3kqZ1CoYPe4dV9g6YHD4NYMCSP1fnqBBiow2khO0XHotr5F_ogKDNAk8UbiMyxCbvLjXLrFBSL-Vw0EgUOZXAbHTmoEjffUQNqgToDakpQLRD8JNnejpnJTO_7x521A-_s2buGZeihQSaD0xo7FeKSidjhwaUYCaqhlrHwqJObQXo6n48EIg4VixIIksdxQlGaCzWaTr0rJbxpscNSiRWmJwWQkrbVnfIp3Bknyg`, "Content-Type": "application/x-www-form-urlencoded" }
 
-    this.http.post<Perro>(this.endpoint, perros).subscribe({
+
+    this.http.post<Perro>(this.endpoint, perros, {headers}).subscribe({
       next: data =>{
         console.log("datos", data);
       },error: error =>{
--- Dogs/proyecto-webdogs/src/app/main-services/raza.service.ts
+++ Dogs/proyecto-webdogs/src/app/main-services/raza.service.ts
@@ -42,9 +42,10 @@ export class RazaService {
   }
 
   getRaza() {
+    const headers = { 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InpqN3h0aEtwX1N1QlloaG4waThWVSJ9.eyJpc3MiOiJodHRwczovL2Rldi01NWFyYWc4ZC51cy5hdXRoMC5jb20vIiwic3ViIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW5AY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMC8iLCJpYXQiOjE2MDcxMzUzNzUsImV4cCI6MTYwNzIyMTc3NSwiYXpwIjoiQjNraU1XTTFpb25yWmlCRnViak9oTDhEZFZ1MkRpNW4iLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.g2R9xXeyKdoOC_9Q1nHCbAwrNqEbXdUjQEup04g1kdkepx0f2Q38imqdjdYi02ZddeWQhca4RnpUzPhcfYR3L7uJemV_OWEwc3UiqHjyFEUhFi3kqZ1CoYPe4dV9g6YHD4NYMCSP1fnqBBiow2khO0XHotr5F_ogKDNAk8UbiMyxCbvLjXLrFBSL-Vw0EgUOZXAbHTmoEjffUQNqgToDakpQLRD8JNnejpnJTO_7x521A-_s2buGZeihQSaD0xo7FeKSidjhwaUYCaqhlrHwqJObQXo6n48EIg4VixIIksdxQlGaCzWaTr0rJbxpscNSiRWmJwWQkrbVnfIp3Bknyg`, "Content-Type": "application/x-www-form-urlencoded" }
 
     console.log("en el servicio")
-    return this.http.get<object[]>(this.endpoint).pipe(retry(3),catchError(this.handleError));
+    return this.http.get<object[]>(this.endpoint, {headers}).pipe(retry(3),catchError(this.handleError));
   }
 
 }
